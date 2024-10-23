f45df81fffbe783988f94ab72f4ecc7ec34a4951
Ryan Lay <30853424+totally-not-frito-lays@users.noreply.github.com>
2024-10-23 13:45:26 -0500

    added first party examples straight from the website

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import { createSwapy } from 'swapy'
 
+// First demo
 const container = document.querySelector('.swp-container')
 
 const swapy = createSwapy(container, {
@@ -11,28 +12,52 @@ swapy.onSwap((event) => {
   console.log(event.data.object);
   console.log(event.data.array);
   console.log(event.data.map);
-
-  // event.data.object:
-  // {
-  //   'foo': 'a',
-  //   'bar': 'b',
-  //   'baz': 'c'
-  // }
-
-  // event.data.array:
-  // [
-  //   { slot: 'foo', item: 'a' },
-  //   { slot: 'bar', item: 'b' },
-  //   { slot: 'baz', item: 'c' }
-  // ]
-
-  // event.data.map:
-  // Map(3) {
-  // 'foo' => 'a',
-  // 'bar' => 'b',
-  // 'baz' => 'c'
-  // }
 })
+
+// Original demo
+const container1 = document.querySelector('.container-1');
+const container2 = document.querySelector('.container-2');
+const enableInput = document.querySelector('#enable') as HTMLInputElement;
+
+const swapy1 = createSwapy(container1, {
+  animation: 'dynamic',
+  swapMode: 'drop',
+});
+
+const swapy2 = createSwapy(container2, {
+  animation: 'spring',
+});
+
+swapy2.onSwap((event) => {
+  console.log('swapy2 event', event.data);
+});
+
+swapy2.onSwapStart(() =>{
+  console.log('swapy2 start');
+})
+
+swapy2.onSwapEnd(() =>{
+  console.log('swapy2 end');
+});
+
+swapy1.enable(false);
+swapy1.onSwap((event) => {
+  console.log('event.map',event.data.map);
+  console.log('event.array',event.data.array);
+  console.log('event.object',event.data.object);
+});
+
+swapy1.onSwap(() => {
+  console.log('swap started');
+});
+
+swapy1.onSwapEnd((event) => {
+  console.log('swap ended', event);
+});
+
+enableInput.addEventListener('change', () => {
+  swapy1.enable(enableInput.checked);
+});
 
 // You can disable and enable it anytime you want
 swapy.enable(true)
